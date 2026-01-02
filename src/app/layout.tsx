11853f3febe1ac7a03d2b5bd4e155386e4231235
KennethGVM <kenethmartinez20@gmail.com>
2026-01-02 00:09:35 -0600

    metadata layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,13 @@ import "./globals.css";
 import { roboto } from "@/fonts/fonts";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://nextbot.vercel.app"),
   title: "NexBot - AI Web Development Assistant",
   description:
     "Get instant answers to your Frontend, Backend, and Performance optimization questions.",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+  authors: {
+    name: "Kenneth Vanegas",
+    url: "https://github.com/KennethGVM",
   },
 };
 
@@ -30,11 +18,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-   return (
+  return (
     <html lang="es">
-      <body className={roboto.className}>
-        {children}
-      </body>
+      <body className={roboto.className}>{children}</body>
     </html>
-  )
+  );
 }
